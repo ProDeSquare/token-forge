cf4fe6e5fb0cd1d3869743a8402d2066ed6979e0
Hamza Mughal <hamza@prodesquare.com>
2025-07-12 22:43:41 +0500

    token models

---
+++ src/model.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Header {
+    pub alg: String,
+    pub typ: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Claims {
+    pub iat: i64,
+    pub exp: Option<i64>,
+
+    #[serde(flatten)]
+    pub payload: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenRequest {
+    pub payload: HashMap<String, serde_json::Value>,
+}
